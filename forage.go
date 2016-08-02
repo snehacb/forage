b07d679e0b4c7f47e21f0836221efef9d7f0f161
Sneha Chandrababu <sneha.chandrababu@gmail.com>
2016-08-01 22:23:28 -0700

    adding forage package

--- forage.go
+++ forage.go
@@ -1,12 +1,17 @@
-package main
+package forager
 
 import (
 "fmt"
 gowit "github.com/snehacb/forage/gowit"
+forager "github.com/snehacb/forage/forager"
 )
 
 func main() {
   fmt.Println("Forager at your service!")
-  fmt.Println("GetRequest")
-  gowit.GetRequest()
+  fmt.Println("Send user message to wit.ai app")
+  witrequest := gowit.SendUserMessage("hi")
+  witresponse := forager.ProcessJsonRequest(witrequest)
+  if !witresponse {
+    fmt.Println("We are done here!")
+  }
 }
--- gowit/gowit.go
+++ gowit/gowit.go
@@ -3,20 +3,39 @@ package gowit
 import (
 "fmt"
 gorequest "github.com/parnurzeal/gorequest"
+"os"
+
 )
 
 var url string
+var token string
 
-func GetRequest() {
+func init() {
+  url = "https://api.wit.ai/message?v=20160702&q="
+  token = os.Getenv("WITTOKEN")
+}
+
+func SendUserMessage(msg string) string {
   request := gorequest.New()
-  resp, body, errs := request.Get(url).End()
+  /* initialize token and url */
+  tokenstr := "Bearer "
+  tokenstr += token
+  urlstr := url
+  urlstr += msg
+  fmt.Printf("Token = %v, Url = %v\n", tokenstr, urlstr)
+  /* send message from user to wit.ai app */
+  resp, body, errs := request.Get(urlstr).Set("Authorization", tokenstr).End()
+
   if resp != nil {
-    fmt.Println(body)
+    fmt.Printf("%v \n", body)
+    fmt.Printf("%T, %v \n", resp, resp)
   } else if errs != nil {
     fmt.Println(errs)
   }
+  fmt.Printf("%v\n", resp)
+  return "abc"
 }
 
-func init() {
-  url = "https://hub.docker.com/v2/search/repositories/?page=1&query=sc&is_official=1&page_size=20"
+func ProcessUserMessage(msg string) {
+
 }
