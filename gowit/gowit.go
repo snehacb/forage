7a3799c1a466fae94674920aebaa21fb83b02aaa
Arunan Rabindran <arunan@docker.com>
2016-08-01 23:26:58 -0700

    Add types for parsing Message JSON

--- gowit/gowit.go
+++ gowit/gowit.go
@@ -1,37 +1,37 @@
 package gowit
 
 import (
-"fmt"
-gorequest "github.com/parnurzeal/gorequest"
-"os"
+	"fmt"
+	"os"
 
+	gorequest "github.com/parnurzeal/gorequest"
 )
 
 var url string
 var token string
 
 func init() {
-  url = "https://api.wit.ai/message?v=20160702&q="
-  token = os.Getenv("WITTOKEN")
+	url = "https://api.wit.ai/message?v=20160702&q="
+	token = os.Getenv("WITTOKEN")
 }
 
 func PingBot(msg string) string {
-  request := gorequest.New()
-  /* initialize token and url */
-  tokenstr := "Bearer "
-  tokenstr += token
-  urlstr := url
-  urlstr += msg
-  fmt.Printf("Token = %v, Url Name = %v\n", tokenstr, urlstr)
-  /* send message from user to wit.ai app */
-  resp, body, errs := request.Get(urlstr).Set("Authorization", tokenstr).End()
+	request := gorequest.New()
+	/* initialize token and url */
+	tokenstr := "Bearer "
+	tokenstr += token
+	urlstr := url
+	urlstr += msg
+	fmt.Printf("Token = %v, Url Name = %v\n", tokenstr, urlstr)
+	/* send message from user to wit.ai app */
+	resp, body, errs := request.Get(urlstr).Set("Authorization", tokenstr).End()
 
-  if resp != nil {
-    fmt.Printf("Response %T, %v \n", resp, resp)
-  } else if errs != nil {
-    fmt.Printf("Errors %T, %v \n", errs, errs)
-  }
-  return body
+	if resp != nil {
+		fmt.Printf("Response %T, %v \n", resp, resp)
+	} else if errs != nil {
+		fmt.Printf("Errors %T, %v \n", errs, errs)
+	}
+	return body
 }
 
 func ProcessUserMessage(msg string) {
--- parser/parser.go
+++ parser/parser.go
@@ -1,9 +1,9 @@
 package parser
 
 import (
-  "fmt"
-
+	"fmt"
 )
+
 /* SAMPLE JSON REQUEST
 string, {
   "msg_id" : "084a2ad7-eee0-4b28-9be4-7203d363c1eb",
@@ -15,17 +15,37 @@ string, {
     } ]
   }
 } */
-type Json struct {
 
+type Value struct {
+	Expressions []string `json:"expressions"`
+	Value       string   `json:"value"`
+}
+
+type Entity struct {
+	Lang    string   `json:"lang"`
+	Closed  bool     `json:"closed"`
+	Exotic  bool     `json:"exotic"`
+	Value   string   `json:"value"`
+	Values  []*Value `json:"values"`
+	Builtin bool     `json:"builtin"`
+	Doc     string   `json:"doc"`
+	Name    string   `json:"name"`
+	ID      string   `json:"id"`
+}
+
+type Message struct {
+	ID       string    `json:"msg_id"`
+	Text     string    `json:"_text"`
+	Entities []*Entity `json:"entities"`
 }
 
 func init() {
 
 }
 
-func ProcessJsonRequest( request string) bool{
-  if request != "" {
-    fmt.Printf("To Marshal: %v", request)
-  }
-  return false
+func ProcessJsonRequest(request string) bool {
+	if request != "" {
+		fmt.Printf("To Marshal: %v", request)
+	}
+	return false
 }
